1cb49c2078627c579d04c69ce5c07ac205b3e257
20btrantanella <20btrantanella@WA-248-9579.schools.westford-ma.gov>
2019-11-18 13:55:54 -0500

    11/18 class

--- LibraryProgram/src/LibraryGUI.java
+++ LibraryProgram/src/LibraryGUI.java
@@ -10,6 +10,37 @@ public class LibraryGUI extends GBFrame {
 		frm.setVisible(true);
 	}
 	
-	JButton
+	JButton newbookButton = addButton("New Book", 1,1,1,1);
+	JButton searchButton = addButton("Search",1,2,1,1);
+	JButton loanButton = addButton("Loan a book",2,1,1,1);
+	JButton returnButton = addButton("Return a book",2,2,1,1);
+	JButton printBorrowedButton = addButton("Borrowed books",3,1,1,1);
+	JButton printOverdueButton = addButton("Overdue books",3,2,1,1);
+	
+	public void buttonClicked(JButton buttonObj) {
+		if (buttonObj == newbookButton) {
+			
+		}
+		
+		if (buttonObj == searchButton) {
+			
+		}
+		
+		if (buttonObj == loanButton) {
+			
+		}
+		
+		if (buttonObj == returnButton) {
+			
+		}
+		
+		if (buttonObj == printBorrowedButton) {
+			
+		}
+		
+		if (buttonObj == printOverdueButton) {
+			
+		}
+	}
 
 }
