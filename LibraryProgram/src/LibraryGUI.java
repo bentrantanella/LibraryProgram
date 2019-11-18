12c60d08a26bcf14dcbba8dcf18272380fac2194
20btrantanella <20btrantanella@WA-248-9579.schools.westford-ma.gov>
2019-11-18 13:31:11 -0500

    11/18 am

---
+++ LibraryProgram/src/LibraryGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import BreezySwing.*;
+
+public class LibraryGUI extends GBFrame {
+	
+	public static void main(String[] args) {
+		JFrame frm = new LibraryGUI();
+		frm.setTitle("Ben's Library");
+		frm.setSize(300, 300);
+		frm.setVisible(true);
+	}
+	
+	JButton
+
+}
