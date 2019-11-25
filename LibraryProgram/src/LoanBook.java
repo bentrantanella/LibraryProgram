1477d15b4cfc66185de6934c7ec8e645aaebc140
charlie <charlie@CMDS66.cmds.com>
2019-11-24 21:54:12 -0500

    11/24 pm

--- LibraryProgram/src/LoanBook.java
+++ LibraryProgram/src/LoanBook.java
@@ -3,19 +3,39 @@ import BreezySwing.*;
 import java.util.ArrayList;
 
 public class LoanBook extends GBDialog {
+	private ArrayList<Library> abook = new ArrayList<Library>();
 	
 	public LoanBook(JFrame parent, ArrayList<Library> book) {
 		super (parent);
 		setTitle("New Book");
 		setSize(300,300);
 		setDlgCloseIndicator ("a");
-		
+		this.abook = book;
 	}
 	
 	JLabel loanLabel = addLabel("Enter the title of the book you wish to loan out",1,1,2,1);
 	JTextField searchField = addTextField("",2,1,1,1);
 	JButton searchButton  = addButton("Search",2,2,1,1);
 	
-	
-
+	public void buttonClicked(JButton buttonObj) {
+		if (buttonObj == searchButton) {
+			String title = loanLabel.getText();
+			
+			String[] booktitles = new String[abook.size()];
+			for(int i = 0; i < abook.size(); i++) {
+				Library book = abook.get(i);
+				booktitles[i] = book.getTitle();
+			}
+			boolean found = false;
+			for(int j = 0; j < abook.size(); j++) {
+				if (searchField.getText().equals(booktitles[j]) == true) {
+					Library searchedbook = abook.get(j);
+					found = true;
+					break;
+				}
+				if (found == false)
+					messageBox("There are no books with this title");
+			}
+		}
+	}
 }
--- LibraryProgram/src/date.java
+++ LibraryProgram/src/date.java
@@ -37,4 +37,40 @@ public class date {
 	public void setYear(int y) {
 		year = y;
 	}
+	
+	public void isValid (date newdate) throws InvalidInput {
+		boolean valid = true;
+		int day = newdate.getDay();
+		int month = newdate.getMonth();
+		int year = newdate.getYear();
+		
+		if (month > 12 || month < 0) {
+			throw new InvalidInput("Invalid month");
+		} else if (year < 0) {
+			throw new InvalidInput("Invalid year");
+		}
+		
+		boolean isleap = isLeap(year);
+		
+		if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day > 31) {
+			throw new InvalidInput("Invalid day");
+		} else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+			throw new InvalidInput("Invalid day");
+		} else if ((month == 2 && isleap == true && day > 29) || (month == 2 && isleap == false && day > 28)) {
+			throw new InvalidInput("Invalid day");
+		}
+		
+		
+	}
+	
+	private boolean isLeap (int year) {
+		boolean leap = false;
+		if (year % 100 == 0 && year % 400 == 0) 
+			leap = true;
+		else if (year % 100 != 0 && year % 4 == 0)
+			leap = true;
+		else leap = false;
+		
+		return leap;
+	}
 }
